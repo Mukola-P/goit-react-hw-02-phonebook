a55ec89ebd86cf4df0dceda83ff89d293bfa74d3
Mukola-P <proxorov.mukola@gmail.com>
2023-01-22 02:38:36 +0200

    goit-react-hw-02-phonebook

---
+++ src/components/Filter/Filter.styled.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const LabelFilter = styled.label`
+  display: block;
+  margin: 0 auto;
+  text-align: center;
+  font-weight: 500;
+  font-size: 16px;
+  color: #fff;
+`;
+
+export const InputFilter = styled.input`
+  display: block;
+  margin-top: 4px;
+  margin-right: auto;
+  margin-left: auto;
+  outline: none;
+  border: 2px solid #8b8888;
+  border-radius: 4px;
+`;
